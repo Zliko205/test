847c5ed201b36912398b120fcb7deaf3b253b159
Zliko205 <109397298+Zliko205@users.noreply.github.com>
2022-09-22 20:16:51 +0200

    Add files via upload
    +js Modules

---
+++ node.js
@@ -0,0 +1,13 @@
+//this is console log
+console.log(55);
+console.log(52);
+
+//This is varriable not function
+
+H = 12;
+console.log("H");
+
+//This summon max/min value in the JS
+
+MAX_VALUE;
+MIN_VALUE;
---
+++ node.ts
@@ -0,0 +1,16 @@
+//this is console log
+
+console.log(55)
+console.log(52)
+
+console.log(5)
+
+//Tohle by měl být součet v TS ale nefunguje
+
+
+function soucet (num1: number , num2: number){
+  return num1 + num2
+}
+
+cons vysledek = soucet("5", 6)
+console.log(vysledek)
